e24d07bb022e20b484c9fc13527c96a3c2fe967a
Eduardo Canda <eduardocandac@gmail.com>
2021-03-18 23:22:01 -0300

    Utilizando design by contract no CreateTodoCommad

--- Todo.Domain/Commands/CreateTodoCommand.cs
+++ Todo.Domain/Commands/CreateTodoCommand.cs
@@ -1,9 +1,11 @@
 using System;
+using Flunt.Notifications;
+using Flunt.Validations;
 using Todo.Domain.Commands.Contracts;
 
 namespace Todo.Domain.Commands
 {
-    public class CreateTodoCommand : ICommand
+    public class CreateTodoCommand : Notifiable, ICommand
     {
         public CreateTodoCommand() { }
 
@@ -13,14 +15,18 @@ namespace Todo.Domain.Commands
             User = user;
             Date = date;
         }
-
         public string Title { get; set; }
         public string User { get; set; }
         public DateTime Date { get; set; }
 
         public void Validate()
         {
-            throw new NotImplementedException();
+            AddNotifications(
+                new Contract()
+                    .Requires()
+                    .HasMinLen(Title, 3, "Title", "Por favor, descreva melhor esta tarefa!")
+                    .HasMinLen(User, 6, "User", "Usuário inválido")
+            );
         }
     }
 }
