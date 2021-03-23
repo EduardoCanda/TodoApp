307e059c1cf9d71fc185dfdecdbd82ded7b1fa09
Eduardo Canda <eduardocandac@gmail.com>
2021-03-22 23:57:35 -0300

    Corrigindo o nome parâmetro dos métodos da interface IHandler

--- Todo.Domain/Handlers/Contracts/IHandler.cs
+++ Todo.Domain/Handlers/Contracts/IHandler.cs
@@ -4,6 +4,6 @@ namespace Todo.Domain.Handlers
 {
     public interface IHandler<T> where T : ICommand
     {
-        ICommandResult Handle(T commmand);
+        ICommandResult Handle(T command);
     }
 }
