3bd54609b7f6633d2cb385142839ea689fcd5e00
Eduardo Canda <eduardocandac@gmail.com>
2021-03-24 17:30:43 -0300

    Testando as queries

--- Todo.Domain.Tests/QueryTests/TodoQueryTests.cs
+++ Todo.Domain.Tests/QueryTests/TodoQueryTests.cs
@@ -18,17 +18,16 @@ namespace Todo.Domain.Tests.QueryTests
             _items.Add(new TodoItem("Tarefa 1", "UsuarioA", DateTime.Now));
             _items.Add(new TodoItem("Tarefa 2", "UsuarioB", DateTime.Now));
             _items.Add(new TodoItem("Tarefa 3", "UsuarioC", DateTime.Now));
-            _items.Add(new TodoItem("Tarefa 4", "UsuarioD", DateTime.Now));
+            _items.Add(new TodoItem("Tarefa 4", "eduardocanda", DateTime.Now));
             _items.Add(new TodoItem("Tarefa 5", "eduardocanda", DateTime.Now));
-            _items.Add(new TodoItem("Tarefa 6", "eduardocanda", DateTime.Now));
         }
 
         [TestMethod]
         public void Deve_retornar_tarefas_apenas_do_usuario_eduardocanda()
         {
-            // var result = _items.Where(TodoQueries.GetAll("eduardocanda"));
+            var result = _items.AsQueryable().Where(TodoQueries.GetAll("eduardocanda"));
             
-            Assert.Fail();
+            Assert.AreEqual(2, result.Count());
         }
 
     }
